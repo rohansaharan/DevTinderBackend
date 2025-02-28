4d861942ee6ff994e200d0aa2fdcf1a397504a9b
Rohan Saharan <saharanrohan4@gmail.com>
2025-02-28 17:35:59 +0530

    added crud operations

--- src/app.js
+++ src/app.js
@@ -4,16 +4,30 @@ const express = require("express");
 
 const app = express();
 
+app.get("/abc/:xyz/:qwe", (req, res) => {
+  console.log(req.params);
+  console.log(req.query);
+  res.send("Hi from abc");
+});
+
+app.get("/user", (req, res) => {
+  res.send("Fetched User Info Successfully!!");
+});
+
+app.post("/user", (req, res) => {
+  res.send("Post user data Successfully!!");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("Updated user Info Successfully!");
+});
+
+app.delete("/user", function (req, res) {
+  res.send("Deleted user Info Successfully!!");
+});
+
 app.use("/test", function (req, res) {
   res.send("You are in testing route");
-});
-
-app.use("/hello", (req, res) => {
-  res.send("Hello!!");
-});
-
-app.use("/", function (req, res) {
-  res.send("Welcome to the Dashboard!");
 });
 
 app.listen(3000, () => {
